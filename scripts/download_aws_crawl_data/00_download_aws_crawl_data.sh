4bdf00108eff99393997e682a0b1317045207ce7
sagarlimbu0 <sagar.limbu.427@my.csun.edu>
2026-02-16 18:09:22 -0800

    update code on downloading gz files

--- scripts/download_aws_crawl_data/00_download_aws_crawl_data.sh
+++ scripts/download_aws_crawl_data/00_download_aws_crawl_data.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+FILE_NAME="cdx-00015.gz"
 
-aws s3 cp s3://commoncrawl/cc-index/collections/CC-MAIN-2021-04/indexes/cdx-00015.gz data/
+aws s3 cp s3://commoncrawl/cc-index/collections/CC-MAIN-2021-04/indexes/$FILE_NAME data/$FILE_NAME
